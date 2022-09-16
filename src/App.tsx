7b35504e0f4d4d1cca615c592255ec3bfc5e325c
Willem Verbuyst <wverbuyst@meditools.nl>
2022-09-16 19:15:10 +0200

    feat: add survey js

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useCallback } from "react";
+
+import "survey-core/defaultV2.min.css";
+import { StylesManager, Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+
+import "./App.css";
+
+StylesManager.applyTheme("defaultV2");
+
+const surveyJson = {
+  elements: [
+    {
+      name: "FirstName",
+      title: "Enter your first name:",
+      type: "text",
+    },
+    {
+      name: "LastName",
+      title: "Enter your last name:",
+      type: "text",
+    },
+  ],
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const survey = new Model(surveyJson);
+  survey.focusFirstQuestionAutomatic = false;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const alertResults = useCallback((sender: { [key: string]: any }) => {
+    const results = JSON.stringify(sender.data);
+    alert(results);
+  }, []);
+
+  survey.onComplete.add(alertResults);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="display">
+      <Survey model={survey} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
