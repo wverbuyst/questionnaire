4a3ee1577ad1345248f9d0c8b297dece0aa1ce68
Willem Verbuyst <wverbuyst@meditools.nl>
2022-09-16 20:11:03 +0200

    feat: serve json

--- server/main.go
+++ server/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+type Element struct {
+	Name  string
+	Title string
+	Type  string
+}
+
+type Questionnaire struct {
+	Elements []Element
+}
+
 func getQuestionnaire(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, gin.H{"message": "here's a questionnaire for ya"})
+	content, err := ioutil.ReadFile("./questionnaire.json")
+	if err != nil {
+		log.Fatal("Error when opening file: ", err)
+	}
+
+	var payload Questionnaire
+	err = json.Unmarshal(content, &payload)
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
+	}
+
+	context.IndentedJSON(http.StatusOK, payload)
 }
 
 func welcomeWorld(context *gin.Context) {
