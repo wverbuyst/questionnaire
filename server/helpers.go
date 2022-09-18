f645065fb636233bd2290ebe8d87647e47aeebd5
Willem Verbuyst <wverbuyst@meditools.nl>
2022-09-18 04:55:39 +0200

    chore: centralize helper fns

--- server/helpers.go
+++ server/helpers.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"errors"
+	"log"
+)
 
 func checkResourceType(r Questionnaire, rt string) {
 	if r.ResourceType != rt {
@@ -17,4 +20,35 @@ func getTextForHtml(r Questionnaire) Elements {
 	}
 	es.Elements = append(es.Elements, e)
 	return es
+}
+
+func getType(t string) (string, error) {
+	switch t {
+	case "boolean":
+		return "boolean", nil
+	case "choice":
+		return "radiogroup", nil
+	case "date":
+		return "text", nil
+	case "dateTime":
+		return "text", nil
+	case "decimal":
+		return "text", nil
+	case "display":
+		return "html", nil
+	case "group":
+		return "panel", nil
+	case "integer":
+		return "text", nil
+	case "string":
+		return "text", nil
+	case "text":
+		return "comment", nil
+	case "time":
+		return "text", nil
+	case "url":
+		return "text", nil
+	default:
+		return "", errors.New("Unhandled value[x] in answerOption")
+	}
 }
--- server/server.sh
+++ server/server.sh
@@ -2,5 +2,4 @@ go run \
 main.go \
 fhirModel.go \
 helpers.go \
-question.go \
 surveyJSModel.go
