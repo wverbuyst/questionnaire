ea8a0327cc5e2bb562652ae50e36765e9ab1252a
Willem <91901507+wverbuyst@users.noreply.github.com>
2025-01-15 17:23:58 +0100

    chore: update react, ts and vite in client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,20 +3,17 @@ import './index.css'
 import { createOvermind } from 'overmind'
 import { Provider } from 'overmind-react'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 import { config } from './state'
 
 const overmind = createOvermind(config)
 
-const rootElement = document.getElementById('root')
-
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider value={overmind}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 )
